9c01167b9607e9c5f7760143adc73a8dc1e3890d
Koen <koen@pop-os.localdomain>
2023-06-20 12:49:09 +0200

    Added website.

---
+++ website/js/script.js
@@ -0,0 +1,13 @@
+/*
+Name:           App Landing
+Written by:     Okler Themes - (http://www.okler.net)
+Theme Version:  9.9.3
+*/
+
+(function( $ ) {
+	
+	'use strict';
+
+	
+
+}).apply( this, [ jQuery ]);
