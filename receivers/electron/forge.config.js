e9b7d288fc8a7f5a1503d95cd31b8d47f39bda62
Michael Hollister <michael@futo.org>
2024-10-02 10:53:02 -0500

    Gitlab CI Testing

--- receivers/electron/forge.config.js
+++ receivers/electron/forge.config.js
@@ -4,7 +4,9 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    osxSign: {},
+    osxSign: {
+      "identity": process.env.FCAST_APPLE_SIGN_ID
+    },
     osxNotarize: {
       appleApiKey: process.env.FCAST_APPLE_API_KEY,
       appleApiKeyId: process.env.FCAST_APPLE_API_KEY_ID,
