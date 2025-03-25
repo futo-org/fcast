428cba044da2edfd197fc59a41a66dd452b1a341
Michael Hollister <michael@futo.org>
2025-03-24 22:03:04 -0500

    Testing potential certificate parse error fix

--- receivers/tizen/scripts/build_local.sh
+++ receivers/tizen/scripts/build_local.sh
@@ -9,5 +9,5 @@ cd FCastReceiver
 tizen build-web -- .
 cd .buildResult
 tizen package -t wgt -s default -- .
-tizen package -t wgt -s default -r ../../FCastReceiverService/bin/Release/netcoreapp2.1/com.futo.FCastReceiverService-1.0.0.tpk -- FCastReceiver.wgt
+tizen package -t wgt -s default -r ../../FCastReceiverService/bin/Release/netcoreapp2.1/com.futo.fcastreceiverservice-1.0.0.tpk -- FCastReceiver.wgt
 cd ../../
--- receivers/tizen/scripts/debug.bat
+++ receivers/tizen/scripts/debug.bat
@@ -1,13 +1,13 @@
 @REM Local development debug script
 cmd /C tizen install -n FCastReceiver.wgt -t UN43DU7200FXZA -- FCastReceiver/.buildResult
-cmd /C C:\tizen-studio\tools\sdb.exe -s 192.168.0.218:26101 shell 0 debug qL5oFoTHoJ.FCastReceiver
+cmd /C C:\tizen-studio\tools\sdb.exe -s 192.168.0.218:26101 shell 0 debug ql5ofothoj.fcastreceiver
 
 @REM Emulators
 @REM cmd /C tizen install -n FCastReceiver/.buildResult/FCastReceiver.wgt -t T-samsung-9.0-x86
-@REM cmd /C C:\tizen-studio\tools\sdb.exe -s emulator-26101 shell 0 debug qL5oFoTHoJ.FCastReceiver
+@REM cmd /C C:\tizen-studio\tools\sdb.exe -s emulator-26101 shell 0 debug ql5ofothoj.fcastreceiver
 
 @REM Samsung remote lab
 @REM cmd /C tizen install -n FCastReceiver.wgt -t QN55Q89RAFXKR -- FCastReceiver/.buildResult
-@REM cmd /C C:\tizen-studio\tools\sdb.exe -s 127.0.0.1:52513 shell 0 debug qL5oFoTHoJ.FCastReceiver
+@REM cmd /C C:\tizen-studio\tools\sdb.exe -s 127.0.0.1:52513 shell 0 debug ql5ofothoj.fcastreceiver
 @REM C:\tizen-studio\tools\sdb.exe forward tcp:34445 tcp:34445
 
