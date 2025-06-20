3cab714b717b52f0e194b7f8f7976ef5ba862954
Michael Hollister <michael@futo.org>
2025-06-20 11:54:35 -0500

    Receivers: Fix mdns internal buffer overflow error and advertisement crash

--- receivers/common/web/DiscoveryService.ts
+++ receivers/common/web/DiscoveryService.ts
@@ -1,6 +1,7 @@
-import mdns from 'modules/mdns-js';
+import mdns from 'modules/@futo/mdns-js';
 import { Logger, LoggerType } from 'common/Logger';
-import { getComputerName } from 'src/Main';
+import { getAppName, getAppVersion, getComputerName } from 'src/Main';
+import { PROTOCOL_VERSION } from 'common/Packets';
 const logger = new Logger('DiscoveryService', LoggerType.BACKEND);
 
 export class DiscoveryService {
@@ -16,10 +17,19 @@ export class DiscoveryService {
         logger.info(`Discovery service started: ${name}`);
 
         // Note that txt field must be populated, otherwise certain mdns stacks have undefined behavior/issues
-        // when connecting to the receiver
-        this.serviceTcp = mdns.createAdvertisement(mdns.tcp('_fcast'), 46899, { name: name, txt: {} });
+        // when connecting to the receiver. Also mdns-js internally gets a lot of parsing errors when txt records
+        // are not defined.
+        this.serviceTcp = mdns.createAdvertisement(mdns.tcp('_fcast'), 46899, { name: name, txt: {
+            version: PROTOCOL_VERSION,
+            appName: getAppName(),
+            appVersion: getAppVersion(),
+        } });
         this.serviceTcp.start();
-        this.serviceWs = mdns.createAdvertisement(mdns.tcp('_fcast-ws'), 46898, { name: name, txt: {} });
+        this.serviceWs = mdns.createAdvertisement(mdns.tcp('_fcast-ws'), 46898, { name: name, txt: {
+            version: PROTOCOL_VERSION,
+            appName: getAppName(),
+            appVersion: getAppVersion(),
+        } });
         this.serviceWs.start();
     }
 
