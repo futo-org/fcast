2aee1a15c5bc7b41c4754a9a5099fd3fc7743444
Marcus Hanestad <marlhan@proton.me>
2025-08-12 11:59:20 +0200

    sender-sdk(rust): implement Debug for device info and protocol type

--- sender-sdk/fcast-sender-sdk/src/device.rs
+++ sender-sdk/fcast-sender-sdk/src/device.rs
@@ -14,6 +14,7 @@ pub enum DeviceConnectionState {
 }
 
 #[cfg_attr(feature = "uniffi", derive(uniffi::Enum))]
+#[derive(Debug)]
 pub enum ProtocolType {
     #[cfg(feature = "chromecast")]
     Chromecast,
@@ -29,6 +30,7 @@ pub(crate) fn ips_to_socket_addrs(ips: &[IpAddr], port: u16) -> Vec<SocketAddr> 
 }
 
 #[cfg_attr(feature = "uniffi", derive(uniffi::Record))]
+#[derive(Debug)]
 pub struct DeviceInfo {
     pub name: String,
     pub r#type: ProtocolType,
