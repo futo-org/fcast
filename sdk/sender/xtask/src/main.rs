692434d0326cc9afe8c0faa3092ca7f97fd03329
Marcus Hanestad <marlhan@proton.me>
2025-08-20 12:26:02 +0200

    sender-sdk: remove old outdated comment

--- sdk/sender/xtask/src/main.rs
+++ sdk/sender/xtask/src/main.rs
@@ -9,19 +9,6 @@ use xtask::{
 #[derive(Subcommand)]
 enum Command {
     Kotlin(kotlin::KotlinArgs),
-    // GenerateIos, // TODO
-    // # https://github.com/Tehnix/template-mobile-wasm
-    // # export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer/
-    // # cargo build -p fcast
-    // # cargo run --bin uniffi-bindgen generate --library target/debug/libfcast.dylib --language swift --out-dir uniffi-out
-    // # mv uniffi-out/fcastFFI.modulemap uniffi-out/fcast.modulemap
-    // # cargo build -p fcast --target=aarch64-apple-ios-sim
-    // # cargo run --bin uniffi-bindgen generate --library target/aarch64-apple-ios-sim/debug/libfcast.dylib --language swift --out-dir uniffi-out
-    // # mv uniffi-out/fcastFFI.modulemap uniffi-out/fcast.modulemap
-    // # rm -rf ios/FCast.xcframework
-    // # xcodebuild -create-xcframework -library target/aarch64-apple-ios-sim/debug/libfcast.a -headers uniffi-out \
-    //     -output ios/FCast.xcframework
-    // # cp uniffi-out/fcast.swift sender-ios/Generated
     Swift(swift::SwiftArgs),
     GenerateIos,
     Hack,
