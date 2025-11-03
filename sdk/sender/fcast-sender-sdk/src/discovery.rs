61495fd65329bf39be4dd08b0a0f7d34795abff9
Marcus Hanestad <marlhan@proton.me>
2025-11-03 10:30:00 +0100

    sender-sdk: emit device removed event with same name as device available

--- sdk/sender/fcast-sender-sdk/src/discovery.rs
+++ sdk/sender/fcast-sender-sdk/src/discovery.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 
 use anyhow::Context;
@@ -16,29 +16,11 @@ pub const FCAST_MDNS_SERVICE_NAME: &str = "_fcast._tcp.local.";
 #[cfg(feature = "chromecast")]
 pub const CHROMECAST_MDNS_SERVICE_NAME: &str = "_googlecast._tcp.local.";
 
-#[cfg(feature = "fcast")]
-fn handle_fcast_mdns_resolved(
-    devices: &mut HashSet<String>,
-    service_info: mdns_sd::ServiceInfo,
-    event_handler: &Arc<dyn DeviceDiscovererEventHandler>,
-) {
-    debug!("Receiver added: {service_info:?}");
-    let mut name = service_info.get_fullname().to_string();
-    if let Some(stripped) = name.strip_suffix(&format!(".{FCAST_MDNS_SERVICE_NAME}")) {
-        name = stripped.to_string();
-    }
-    let port = service_info.get_port();
-    let resolved_service = service_info.as_resolved_service();
-    let addresses = scoped_ip_to_custom(&resolved_service.addresses);
-    let device_info = DeviceInfo::fcast(name, addresses, port);
-    let fullname = resolved_service.fullname;
-    if devices.contains(&fullname) {
-        debug!("Updating FCast device `{}`", device_info.name);
-        event_handler.device_changed(device_info);
+fn strip_service_name(fullname: &str, service_name: &str) -> String {
+    if let Some(stripped) = fullname.strip_suffix(&format!(".{service_name}")) {
+        stripped.to_string()
     } else {
-        debug!("New FCast device `{}`", device_info.name);
-        event_handler.device_available(device_info);
-        devices.insert(fullname);
+        fullname.to_string()
     }
 }
 
@@ -75,6 +57,29 @@ fn scoped_ip_to_custom(addrs: &HashSet<ScopedIp>) -> Vec<IpAddr> {
         .collect()
 }
 
+fn service_resolved(
+    devices: &mut HashMap<String, String>,
+    event_handler: &Arc<dyn DeviceDiscovererEventHandler>,
+    service_info: mdns_sd::ServiceInfo,
+    mut device_info: DeviceInfo,
+) {
+    debug!("Receiver added: {service_info:?}");
+    let port = service_info.get_port();
+    let resolved_service = service_info.as_resolved_service();
+    let addresses = scoped_ip_to_custom(&resolved_service.addresses);
+    device_info.port = port;
+    device_info.addresses = addresses;
+    let fullname = resolved_service.fullname;
+    if devices.contains_key(&fullname) {
+        debug!("Updating device `{}`", device_info.name);
+        event_handler.device_changed(device_info);
+    } else {
+        debug!("New device `{}`", device_info.name);
+        event_handler.device_available(device_info.clone());
+        devices.insert(fullname, device_info.name);
+    }
+}
+
 enum Message {
     #[cfg(feature = "fcast")]
     FCastServiceEvent(ServiceEvent),
@@ -87,7 +92,7 @@ pub(crate) async fn discover_devices(
 ) -> anyhow::Result<()> {
     let service_daemon =
         mdns_sd::ServiceDaemon::new().context("Failed to create mDNS ServiceDaemon")?;
-    let mut devices: HashSet<String> = HashSet::new();
+    let mut devices: HashMap<String, String> = HashMap::new();
 
     macro_rules! browse {
         ($mdns:expr, $service:expr) => {
@@ -101,22 +106,6 @@ pub(crate) async fn discover_devices(
     let fcast_mdns_receiver = browse!(service_daemon, FCAST_MDNS_SERVICE_NAME)?;
     #[cfg(feature = "chromecast")]
     let chromecast_mdns_receiver = browse!(service_daemon, CHROMECAST_MDNS_SERVICE_NAME)?;
-
-    macro_rules! handle_service_event {
-        ($event:expr, $on_resolved:expr) => {
-            match $event {
-                mdns_sd::ServiceEvent::ServiceResolved(service_info) => $on_resolved(service_info),
-                mdns_sd::ServiceEvent::ServiceRemoved(_, fullname) => {
-                    if devices.remove(&fullname) {
-                        event_handler.device_removed(fullname);
-                    } else {
-                        debug!("Service `{fullname}` was removed but no device was found");
-                    }
-                }
-                _ => (),
-            }
-        };
-    }
 
     let msg_stream = futures::stream::unfold((), async |_| None::<(Message, ())>);
     tokio::pin!(msg_stream);
@@ -155,35 +144,125 @@ pub(crate) async fn discover_devices(
     while let Some(msg) = msg_stream.next().await {
         match msg {
             #[cfg(feature = "fcast")]
-            Message::FCastServiceEvent(service_event) => {
-                handle_service_event!(service_event, |service_info: mdns_sd::ServiceInfo| {
-                    handle_fcast_mdns_resolved(&mut devices, service_info, &event_handler);
-                })
-            }
+            Message::FCastServiceEvent(service_event) => match service_event {
+                ServiceEvent::ServiceResolved(service_info) => {
+                    let name =
+                        strip_service_name(service_info.get_fullname(), FCAST_MDNS_SERVICE_NAME);
+                    let device_info = DeviceInfo::fcast(name.clone(), vec![], 0);
+                    service_resolved(&mut devices, &event_handler, service_info, device_info);
+                }
+                ServiceEvent::ServiceRemoved(_, fullname) => {
+                    if let Some(name) = devices.remove(&fullname) {
+                        event_handler.device_removed(name);
+                    } else {
+                        debug!("Service `{fullname}` was removed but no device was found");
+                    }
+                }
+                _ => (),
+            },
             #[cfg(feature = "chromecast")]
-            Message::ChromecastServiceEvent(service_event) => {
-                handle_service_event!(service_event, |service_info: mdns_sd::ServiceInfo| {
+            Message::ChromecastServiceEvent(service_event) => match service_event {
+                ServiceEvent::ServiceResolved(service_info) => {
                     let name = service_info
                         .get_property(CHROMECAST_FRIENDLY_NAME_TXT)
                         .map(|name| name.val_str().to_string())
                         .unwrap_or(service_info.get_fullname().to_string());
-                    let port = service_info.get_port();
-                    let resolved_service = service_info.as_resolved_service();
-                    let addresses = scoped_ip_to_custom(&resolved_service.addresses);
-                    let device_info = DeviceInfo::chromecast(name, addresses, port);
-                    let fullname = resolved_service.fullname;
-                    if devices.contains(&fullname) {
-                        debug!("Updating Chromecast device `{}`", device_info.name);
-                        event_handler.device_changed(device_info);
+                    let device_info = DeviceInfo::chromecast(name.clone(), vec![], 0);
+                    service_resolved(&mut devices, &event_handler, service_info, device_info);
+                }
+                ServiceEvent::ServiceRemoved(_, fullname) => {
+                    if let Some(name) = devices.remove(&fullname) {
+                        event_handler.device_removed(name);
                     } else {
-                        debug!("New Chromecast device `{}`", device_info.name);
-                        event_handler.device_available(device_info);
-                        devices.insert(fullname);
+                        debug!("Service `{fullname}` was removed but no device was found");
                     }
-                })
-            }
+                }
+                _ => (),
+            },
         }
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_strip_service_name() {
+        assert_eq!(
+            &strip_service_name(
+                &format!("FCast-receiver.{FCAST_MDNS_SERVICE_NAME}"),
+                FCAST_MDNS_SERVICE_NAME
+            ),
+            "FCast-receiver"
+        );
+        assert_eq!(
+            &strip_service_name("FCast-receiver", FCAST_MDNS_SERVICE_NAME),
+            "FCast-receiver"
+        );
+    }
+
+    #[test]
+    fn test_device_discovery_events() {
+        struct TestingEventHandler {}
+
+        impl DeviceDiscovererEventHandler for TestingEventHandler {
+            fn device_available(&self, _device_info: DeviceInfo) {}
+            fn device_removed(&self, _device_name: String) {}
+            fn device_changed(&self, _device_info: DeviceInfo) {}
+        }
+
+        let handler: Arc<dyn DeviceDiscovererEventHandler> = Arc::new(TestingEventHandler {});
+        let mut devices: HashMap<String, String> = HashMap::new();
+
+        let fcast_dev_info = DeviceInfo::fcast("FCast-receiver".to_string(), vec![], 0);
+        service_resolved(
+            &mut devices,
+            &handler,
+            mdns_sd::ServiceInfo::new(
+                FCAST_MDNS_SERVICE_NAME,
+                "FCast-receiver",
+                "",
+                (),
+                1234,
+                Vec::new(),
+            )
+            .unwrap(),
+            fcast_dev_info,
+        );
+
+        assert_eq!(
+            devices
+                .get(&format!("FCast-receiver.{FCAST_MDNS_SERVICE_NAME}"))
+                .unwrap(),
+            "FCast-receiver"
+        );
+
+        let chromecast_dev_info = DeviceInfo::fcast("Chromecast-receiver".to_string(), vec![], 0);
+        service_resolved(
+            &mut devices,
+            &handler,
+            mdns_sd::ServiceInfo::new(
+                CHROMECAST_MDNS_SERVICE_NAME,
+                "Chromecast-abcdefghijklmnopqrstuvwxyz",
+                "",
+                (),
+                1234,
+                Vec::new(),
+            )
+            .unwrap(),
+            chromecast_dev_info,
+        );
+
+        assert_eq!(
+            devices
+                .get(&format!(
+                    "Chromecast-abcdefghijklmnopqrstuvwxyz.{CHROMECAST_MDNS_SERVICE_NAME}"
+                ))
+                .unwrap(),
+            "Chromecast-receiver"
+        );
+    }
 }
