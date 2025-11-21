0da8f727e5ff08bdb20be2337c1e665d4bdc3e9b
Michael Hollister <michael@futo.org>
2025-11-21 17:43:12 -0600

    Android: Removed unneeded proxy server

--- receivers/android/app/src/main/java/com/futo/fcast/receiver/NetworkService.kt
+++ receivers/android/app/src/main/java/com/futo/fcast/receiver/NetworkService.kt
@@ -35,7 +35,6 @@ import com.futo.fcast.receiver.models.SubscribeEventMessage
 import com.futo.fcast.receiver.models.UnsubscribeEventMessage
 import com.futo.fcast.receiver.models.VersionMessage
 import com.futo.fcast.receiver.models.VolumeUpdateMessage
-import com.futo.fcast.receiver.proxy.ProxyService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
@@ -60,9 +59,7 @@ class NetworkService : Service() {
     lateinit var discoveryService: DiscoveryService
 
     private lateinit var _tcpListenerService: TcpListenerService
-    private lateinit var _proxyService: ProxyService
     private lateinit var _scope: CoroutineScope
-    private var _delayedStart: Boolean = false
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -95,7 +92,6 @@ class NetworkService : Service() {
 
         _tcpListenerService = TcpListenerService(this) { onNewSession(it) }
         ConnectionMonitor(_scope)
-        _proxyService = ProxyService()
 
         val name = "Network Listener Service"
         val descriptionText = "Listening on port ${TcpListenerService.PORT} (TCP)"
@@ -130,11 +126,6 @@ class NetworkService : Service() {
         discoveryService.start()
         _tcpListenerService.start()
         networkWorker.start()
-        if (networkWorker.interfaces.isEmpty()) {
-            _delayedStart = true
-        } else {
-            _proxyService.start()
-        }
 
         instance = this
         Log.i(TAG, "Started NetworkService")
@@ -144,11 +135,6 @@ class NetworkService : Service() {
         MainActivity.instance?.networkChanged()
 
         return START_STICKY
-    }
-
-    fun start() {
-        _delayedStart = false
-        _proxyService.start()
     }
 
     @Suppress("DEPRECATION")
@@ -169,7 +155,6 @@ class NetworkService : Service() {
         discoveryService.stop()
         _tcpListenerService.stop()
 
-        _proxyService.stop()
         networkWorker.stop()
         _scope.cancel()
 
@@ -201,13 +186,11 @@ class NetworkService : Service() {
         // Protocol v2 FCast PlayMessage does not contain volume field and could result in the receiver
         // getting out-of-sync with the sender when player windows are closed and re-opened. Volume
         // is cached in the play message when volume is not set in v3 PlayMessage.
-        var rendererMessage = PlayMessage(
+        val rendererMessage = PlayMessage(
             message.container, message.url,
             message.content, message.time, message.volume ?: cachedPlayerVolume,
             message.speed, message.headers, message.metadata
         )
-
-        rendererMessage = ProxyService.proxyPlayIfRequired(rendererMessage)
 
         if (message.container == "application/json") {
             val jsonStr: String =
--- receivers/android/app/src/main/java/com/futo/fcast/receiver/proxy/server/HttpBridge.kt
+++
@@ -1,9 +0,0 @@
-package com.futo.fcast.receiver.proxy.server
-
-@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class HttpGET(val path: String, val contentType: String = "");
-
-@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class HttpPOST(val path: String, val contentType: String = "");
