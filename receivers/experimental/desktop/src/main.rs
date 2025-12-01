c650658c32a40cb8aabcd6bd519a0094c0105540
Marcus Hanestad <marlhan@proton.me>
2025-12-01 09:38:02 +0100

    gst-play

--- receivers/experimental/desktop/src/main.rs
+++ receivers/experimental/desktop/src/main.rs
@@ -1,7 +1,7 @@
 fn main() -> anyhow::Result<()> {
     env_logger::Builder::from_default_env()
-        // .filter_module("receiver", common::default_log_level())
-        // .filter_module("receiver-core", common::default_log_level())
+        .filter_module("receiver-desktop", rcore::common::default_log_level())
+        .filter_module("rcore", rcore::common::default_log_level())
         .init();
 
     if std::env::var("SLINT_BACKEND") == Err(std::env::VarError::NotPresent) {
