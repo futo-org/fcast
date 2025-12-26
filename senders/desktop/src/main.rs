f994f112e988dfd6bf1a475ddd27e3d1ba6276c9
Marcus Hanestad <marlhan@proton.me>
2025-12-26 09:10:09 +0100

    sender: desktop: disable yt-dlp

--- senders/desktop/src/main.rs
+++ senders/desktop/src/main.rs
@@ -1430,7 +1430,7 @@ impl Application {
         gstrsrtp::plugin_register_static()?;
 
         tokio::spawn({
-            let ui_weak = self.ui_weak.clone();
+            // let ui_weak = self.ui_weak.clone();
             async move {
                 let yt_dlp_available = match mcore::yt_dlp::is_yt_dlp_available().await {
                     Ok(p) => p,
@@ -1442,10 +1442,11 @@ impl Application {
 
                 debug!(?yt_dlp_available, "yt-dlp status");
 
-                let _ = ui_weak.upgrade_in_event_loop(move |ui| {
-                    ui.global::<Bridge>()
-                        .set_is_yt_dlp_available(yt_dlp_available);
-                });
+                // TODO: include this when ready
+                // let _ = ui_weak.upgrade_in_event_loop(move |ui| {
+                //     ui.global::<Bridge>()
+                //         .set_is_yt_dlp_available(yt_dlp_available);
+                // });
             }
         });
 
