1885c7043ad52d6765d879da2c76cf171b475bc2
Marcus Hanestad <marlhan@proton.me>
2026-01-04 15:09:43 +0100

    remove gst-plugins-rs dep

--- senders/desktop/fcast-sender.nix
+++ senders/desktop/fcast-sender.nix
@@ -63,8 +63,7 @@ rustPlatform.buildRustPackage rec {
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
     gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad # needed?
-    gst_all_1.gst-plugins-rs # needed?
+    gst_all_1.gst-plugins-bad
     gst_all_1.gst-libav
     glib
     openssl
