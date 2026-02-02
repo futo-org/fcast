45857ebfda70ac484ed17969ac7806b472a22f5e
Michael Hollister <michael@futo.org>
2026-02-02 15:06:22 -0600

    experimental-receiver: android add back all architectures

--- receivers/experimental/android/app/jni/Application.mk
+++ receivers/experimental/android/app/jni/Application.mk
@@ -1,5 +1,3 @@
-# APP_ABI = x86_64 # arm64-v8a
-APP_ABI = arm64-v8a
-# APP_ABI = arm64-v8a
+APP_PLATFORM = 25
+APP_ABI = arm64-v8a x86_64 armeabi-v7a x86
 APP_STL = c++_shared
-APP_PLATFORM=android-24
