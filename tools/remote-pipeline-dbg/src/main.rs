1a774995f314fbe2af127a97b6467aafee722274
Marcus Hanestad <marlhan@proton.me>
2026-02-28 19:44:02 +0100

    dbg: improved UI

--- tools/remote-pipeline-dbg/src/main.rs
+++ tools/remote-pipeline-dbg/src/main.rs
@@ -1,4 +1,6 @@
 #[cfg(feature = "gui")]
+use slint::Model;
+#[cfg(feature = "gui")]
 use std::{
     io::Write,
     net::TcpListener,
@@ -20,15 +22,19 @@ fn run(ui_weak: slint::Weak<MainWindow>) {
             }
         };
 
+        let client_addr = stream
+            .peer_addr()
+            .map(|s| s.to_string())
+            .unwrap_or("n/a".to_owned());
+
         ui_weak
             .upgrade_in_event_loop(|ui| {
                 ui.global::<Bridge>()
                     .set_waiting_state(WaitingState::Receiving);
-                ui.global::<Bridge>()
-                    .set_graph(slint::Image::default());
             })
             .unwrap();
 
+        let now = chrono::Local::now();
         let (data_buf, source, trigger) = remote_pipeline_dbg::read_graph(stream).unwrap();
 
         let mut graphviz = Command::new("dot")
@@ -52,7 +58,6 @@ fn run(ui_weak: slint::Weak<MainWindow>) {
         let width = u16::from_be_bytes([image_data[2], image_data[3]]);
         let height = u16::from_be_bytes([image_data[4], image_data[5]]);
         let argb = &mut image_data[11..];
-        println!("Got image width={width} height={height}");
         assert_eq!(argb.len(), width as usize * height as usize * 4);
         image_swizzle::argb_to_rgba_inplace(argb);
         image_swizzle::rgb0_to_bgrx_inplace(argb);
@@ -63,11 +68,26 @@ fn run(ui_weak: slint::Weak<MainWindow>) {
             height as u32,
         );
         ui_weak
-            .upgrade_in_event_loop(|ui| {
+            .upgrade_in_event_loop(move |ui| {
+                use slint::ToSharedString;
+
                 let img = slint::Image::from_rgba8(buffer);
-                ui.global::<Bridge>().set_graph(img);
-                ui.global::<Bridge>()
-                    .set_waiting_state(WaitingState::Waiting);
+                let bridge = ui.global::<Bridge>();
+                let dumps = bridge.get_dumps();
+                let model = dumps
+                    .as_any()
+                    .downcast_ref::<slint::VecModel<UiGraphDump>>()
+                    .unwrap();
+                model.push(UiGraphDump {
+                    title: now.format("%H:%M:%S").to_shared_string(),
+                    client: client_addr.to_shared_string(),
+                    pipeline: source.to_string().to_shared_string(),
+                    trigger: trigger.to_string().to_shared_string(),
+                    graph: img,
+                });
+                let idx = model.row_count() as i32;
+                bridge.set_selected_dump(idx - 1);
+                bridge.set_waiting_state(WaitingState::Waiting);
             })
             .unwrap();
     }
@@ -81,6 +101,18 @@ fn main() {
         .unwrap();
 
     let ui = MainWindow::new().unwrap();
+
+    let bridge = ui.global::<Bridge>();
+    bridge.set_dumps(std::rc::Rc::new(slint::VecModel::default()).into());
+
+    bridge.on_remove_element(|model: slint::ModelRc<UiGraphDump>, idx: i32| {
+        let model = model
+            .as_any()
+            .downcast_ref::<slint::VecModel<UiGraphDump>>()
+            .unwrap();
+        assert!(idx >= 0);
+        model.remove(idx as usize);
+    });
 
     let ui_weak = ui.as_weak();
     std::thread::spawn(move || {
