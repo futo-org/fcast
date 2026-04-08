ead4148001f7890a58b42c7e9a2d4b1e5df4d1a0
Marcus Hanestad <marlhan@proton.me>
2026-04-08 12:02:36 +0200

    experimental-receiver: split logging into separate module

--- receivers/experimental/rcore/src/lib.rs
+++ receivers/experimental/rcore/src/lib.rs
@@ -57,6 +57,7 @@ mod raop;
 mod user_agent;
 mod video;
 mod gstreamer;
+mod logging;
 
 use crate::{
     fcast::{Operation, ReceiverToSenderMessage, TranslatableMessage},
@@ -1965,24 +1966,6 @@ impl Application {
 }
 
 #[cfg(not(target_os = "android"))]
-fn log_level() -> LevelFilter {
-    match std::env::var("FCAST_LOG") {
-        Ok(level) => match level.to_ascii_lowercase().as_str() {
-            "error" => LevelFilter::ERROR,
-            "warn" => LevelFilter::WARN,
-            "info" => LevelFilter::INFO,
-            "debug" => LevelFilter::DEBUG,
-            "trace" => LevelFilter::TRACE,
-            _ => LevelFilter::OFF,
-        },
-        #[cfg(debug_assertions)]
-        Err(_) => LevelFilter::DEBUG,
-        #[cfg(not(debug_assertions))]
-        Err(_) => LevelFilter::OFF,
-    }
-}
-
-#[cfg(not(target_os = "android"))]
 #[derive(clap::Parser)]
 #[command(version)]
 pub struct CliArgs {
@@ -2017,39 +2000,7 @@ pub fn run(
     #[cfg(target_os = "android")] android_app: slint::android::AndroidApp,
     #[cfg(target_os = "android")] mut platform_event_rx: UnboundedReceiver<Event>,
 ) -> Result<()> {
-    let prev_panic_hook = std::panic::take_hook();
-    std::panic::set_hook(Box::new(move |panic_info| {
-        tracing_panic::panic_hook(panic_info);
-        prev_panic_hook(panic_info);
-    }));
-    tracing_gstreamer::integrate_events();
-    gst::log::remove_default_log_function();
-
-    #[cfg(not(target_os = "android"))]
-    {
-        use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-        let log_level = cli_args.loglevel.unwrap_or(log_level());
-        let filter = tracing_subscriber::filter::Targets::new()
-            .with_target("tracing_gstreamer::callsite", LevelFilter::OFF)
-            .with_target("mdns_sd", LevelFilter::INFO)
-            .with_target("hyper_util", LevelFilter::INFO)
-            .with_target("h2", LevelFilter::INFO)
-            .with_target("winit", LevelFilter::INFO)
-            .with_default(log_level);
-        let fmt_layer = tracing_subscriber::fmt::layer();
-        gst::log::set_default_threshold(gst::DebugLevel::Warning);
-        let registry = tracing_subscriber::registry().with(fmt_layer).with(filter);
-        #[cfg(feature = "tracy")]
-        let registry = registry.with(tracing_tracy::TracyLayer::default());
-        registry.init();
-    }
-
-    #[cfg(target_os = "android")]
-    {
-        gst::log::set_default_threshold(gst::DebugLevel::Warning);
-        gst::log::set_threshold_for_name("gldebug", gst::DebugLevel::None);
-        gst::log::set_threshold_for_name("video-info", gst::DebugLevel::None);
-    }
+    logging::init(cli_args.loglevel);
 
     let start = std::time::Instant::now();
 
