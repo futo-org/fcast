de72836a4ee7e85a7aede6412b3e62dd56d2387a
Marcus Hanestad <marlhan@proton.me>
2026-06-03 10:10:35 +0200

    repo: remove the deprecated electron receiver

--- receivers/electron/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['<rootDir>/test/**/*.test.ts'],
-    modulePathIgnorePatterns: ["<rootDir>/packaging/fcast/fcast-receiver-linux-x64/resources/app/package.json"],
-};
