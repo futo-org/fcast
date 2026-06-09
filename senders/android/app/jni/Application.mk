fd505a1d2267a0a3e55110bc6e9411a492c21f16
Marcus Hanestad <marcus@futo.org>
2026-06-09 18:11:52 +0000

    ci speed improvements

--- senders/android/app/jni/Application.mk
+++ senders/android/app/jni/Application.mk
@@ -1,3 +1,5 @@
-APP_ABI = arm64-v8a x86_64 armeabi-v7a x86
+# APP_ABI = arm64-v8a x86_64 armeabi-v7a x86
+# TODO: support other architectures once they've been tested
+APP_ABI = arm64-v8a
 APP_STL = c++_shared
 APP_PLATFORM = android-24
