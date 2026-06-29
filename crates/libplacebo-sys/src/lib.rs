5cdc035a93b559c83533e303dd3f3cd1129f353a
Marcus Hanestad <marlhan@proton.me>
2026-06-29 10:12:31 +0200

    crates: libplacebo: relax linting

--- crates/libplacebo-sys/src/lib.rs
+++ crates/libplacebo-sys/src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 #![allow(unnecessary_transmutes)]
+#![allow(suspicious_runtime_symbol_definitions)]
 // Lints
 #![allow(clippy::missing_safety_doc)]
 #![allow(clippy::ptr_offset_with_cast)]
